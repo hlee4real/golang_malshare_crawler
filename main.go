fa4b9d41d0b956d980da1e44ffb7362ea474ff52
Lê Huy Hoàng <lehuyhoang@apples-MacBook-Pro.local>
2022-09-28 10:01:14 +0700

    fix too many open files in system

--- main.go
+++ main.go
@@ -62,63 +62,35 @@ func main() {
 		var strArray1 []string = r1.FindAllString(string(body), -1)
 		// fmt.Println(strArray1)
 		directMd5 := newPath + "/" + "md5.txt"
-		f, err := os.Create(directMd5)
-		if err != nil {
-			log.Fatal(err)
-		}
+		writeFile(str, directMd5, strArray1)
 
-		defer f.Close()
-		for j := 0; j < len(strArray1); j++ {
-			_, err2 := f.WriteString(string(strArray1[j]) + "\n")
-			if err2 != nil {
-				log.Fatal(err2)
-			}
-		}
 		r2, _ := regexp.Compile(`\b[a-fA-F0-9]{40}\b`)
 		var strArray2 []string = r2.FindAllString(string(body), -1)
 		// fmt.Println(strArray2)
 		directSha1 := newPath + "/" + "sha1.txt"
-		f1, err3 := os.Create(directSha1)
-		if err3 != nil {
-			log.Fatal(err3)
-		}
-
-		defer f1.Close()
-		for j := 0; j < len(strArray2); j++ {
-			_, err2 := f1.WriteString(string(strArray2[j]) + "\n")
-			if err2 != nil {
-				log.Fatal(err2)
-			}
-		}
+		writeFile(str, directSha1, strArray2)
 
 		r3, _ := regexp.Compile(`[a-fA-F0-9]{64}`)
 		var strArray3 []string = r3.FindAllString(string(body), -1)
 		// fmt.Println(strArray3)
 		directSha256 := newPath + "/" + "sha256.txt"
-		f3, err4 := os.Create(directSha256)
-		if err4 != nil {
-			log.Fatal(err4)
-		}
+		writeFile(str, directSha256, strArray3)
+	}
+}
 
-		defer f3.Close()
-		for j := 0; j < len(strArray3); j++ {
-			_, err2 := f3.WriteString(string(strArray3[j]) + "\n")
-			if err2 != nil {
-				log.Fatal(err2)
-			}
-		}
+//function to write to file
+
+func writeFile(str string, direct string, strArray []string) {
+	f, err := os.Create(direct)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	// r1, _ := regexp.Compile(`[a-fA-F0-9]{64}`)
-	// var strArray1 []string = r1.FindAllString(string(body1), -1)
-	// fmt.Println(strArray1)
-
-	// r2, _ := regexp.Compile(`\b[a-fA-F0-9]{32}\b`)
-	// var strArray2 []string = r2.FindAllString(string(body1), -1)
-	// fmt.Println(strArray2)
-
-	// r3, _ := regexp.Compile(`\b[a-fA-F0-9]{40}\b`)
-	// var strArray3 []string = r3.FindAllString(string(body1), -1)
-	// fmt.Println(strArray3)
-
+	defer f.Close()
+	for i := 0; i < len(strArray); i++ {
+		_, err2 := f.WriteString(string(strArray[i]) + "\n")
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+	}
 }
