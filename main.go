4310d459b50fd69d6b36cebc482b8d7be9a109a1
Lê Huy Hoàng <lehuyhoang@apples-MacBook-Pro.local>
2022-10-03 10:39:36 +0700

    add go workers

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"io/ioutil"
@@ -12,10 +13,26 @@ import (
 	"strings"
 	"sync"
 	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-type Data struct {
+type Crawler struct {
+	Date   string `json:"date" bson:"date"`
+	Md5    string `json:"md5" bson:"md5"`
+	Sha1   string `json:"sha1" bson:"sha1"`
+	Sha256 string `json:"sha256" bson:"sha256"`
 }
+type Site struct {
+	URL string
+}
+
+// type Result struct {
+// 	URL     string
+// 	message string
+// }
 
 var wg sync.WaitGroup
 
@@ -23,8 +40,6 @@ var wg sync.WaitGroup
 var regexMd5 = regexp.MustCompile(`\b[a-fA-F0-9]{32}\b`)
 var regexSha1 = regexp.MustCompile(`\b[a-fA-F0-9]{40}\b`)
 var regexSha256 = regexp.MustCompile(`\b[a-fA-F0-9]{64}\b`)
-
-// r3, _ := regexp.Compile(`[a-fA-F0-9]{64}`)
 
 func main() {
 	resp, err := http.Get("https://malshare.com/daily/")
@@ -59,8 +74,8 @@ func main() {
 	}
 	t := &http.Transport{
 		Dial: (&net.Dialer{
-			Timeout:   time.Hour * 2,
-			KeepAlive: time.Hour,
+			Timeout:   time.Hour * 2 * 10,
+			KeepAlive: time.Hour * 10,
 		}).Dial,
 		// We use ABSURDLY large keys, and should probably not.
 		// TLSHandshakeTimeout: 60 * time.Second,
@@ -68,58 +83,39 @@ func main() {
 	c := &http.Client{
 		Transport: t,
 	}
-	message := make(chan string, len(strArray))
+	var urls [5300]string
+	for i := 0; i < len(strArray); i++ {
+		urls[i] = fmt.Sprintf("https://malshare.com/daily/%s/malshare_fileList.%s.all.txt", strArray[i], strArray[i])
+		fmt.Println(urls[i])
+	}
+
+	message := make(chan Site, len(strArray)*3)
+	result := make(chan string, len(strArray)*3)
+	for w := 1; w <= 10; w++ {
+		go makeRequest(message, c, urls[:], result)
+	}
+	for _, url := range urls {
+		message <- Site{URL: url}
+	}
+	close(message)
 	for i := 0; i < len(strArray); i++ {
 		newPath := filepath.Join("newmalshare", strNewArray[i])
-
 		err := os.MkdirAll(newPath, os.ModePerm)
 		if err != nil {
 			fmt.Println(err)
 			return
-			// log.Println(err)
 		}
-
-		wg.Add(1)
-		go func(i int, message chan string, c *http.Client, strArray []string) {
-			makeRequest(i, message, c, strArray)
-			// writeFileMd5(newPath, message)
-			// writeFileSha1(newPath, message)
-			// writeFileSha256(newPath, message)
-			wg.Done()
-			// writeFileMd5()
-
-		}(i, message, c, strArray)
-
-		go func(newPath string, message chan string) {
-			// direct = fmt.Sprintf("%s/md5.txt", newPath)
-			writeFileMd5(newPath, message)
-			wg.Done()
-		}(newPath, message)
-		// go writeFileMd5(message, *regexMd5, fmt.Sprintf("%s/sha1.txt", newPath))
-		go func(newPath string, message chan string) {
-			writeFileSha1(newPath, message)
-			wg.Done()
-		}(newPath, message)
-		go func(newPath string, message chan string) {
-			writeFileSha256(newPath, message)
-			wg.Done()
-		}(newPath, message)
-
+		res := <-result
+		go writeFileMd5(newPath, res)
+		go writeFileSha1(newPath, res)
+		go writeFileSha256(newPath, res)
 	}
-	wg.Wait()
 
-	// time.Sleep(time.Minute * 5)
 }
+func writeFileMd5(newPath string, res string) error {
 
-//function to write to file
-
-func writeFileMd5(newPath string, message chan string) error {
-	// r1, _ := regexp.Compile(`\b[a-fA-F0-9]{32}\b`)
-
-	var strArray []string = regexMd5.FindAllString(<-message, -1)
+	var strArray []string = regexMd5.FindAllString(res, -1)
 	direct := fmt.Sprintf("%s/md5.txt", newPath)
-	//filepath.Join("newmalshare", strNewArray[i])
-	//newPath + "/" + "md5.txt"
 	if _, err := os.Stat(direct); os.IsNotExist(err) {
 		f, err := os.Create(direct)
 		if err != nil {
@@ -138,12 +134,11 @@ func writeFileMd5(newPath string, message chan string) error {
 			}
 		}
 	}
-	// defer wg.Done()
 	return nil
 }
 
-func writeFileSha1(newPath string, message chan string) error {
-	var strArray []string = regexSha1.FindAllString(<-message, -1)
+func writeFileSha1(newPath string, res string) error {
+	var strArray []string = regexSha1.FindAllString(res, -1)
 	direct := fmt.Sprintf("%s/sha1.txt", newPath) //newPath + "/" + "sha1.txt"
 	if _, err := os.Stat(direct); os.IsNotExist(err) {
 		f, err := os.Create(direct)
@@ -166,9 +161,9 @@ func writeFileSha1(newPath string, message chan string) error {
 	return nil
 }
 
-func writeFileSha256(newPath string, message chan string) error {
+func writeFileSha256(newPath string, res string) error {
 	// r3, _ := regexp.Compile(`[a-fA-F0-9]{64}`)
-	strArray := regexSha256.FindAllString(<-message, -1)
+	strArray := regexSha256.FindAllString(res, -1)
 	// direct := newPath + "/" + "sha256.txt"
 	direct := fmt.Sprintf("%s/sha256.txt", newPath)
 	if _, err := os.Stat(direct); os.IsNotExist(err) {
@@ -191,21 +186,45 @@ func writeFileSha256(newPath string, message chan string) error {
 	return nil
 }
 
-func makeRequest(i int, message chan string, c *http.Client, strArray []string) {
+func makeRequest(message <-chan Site, c *http.Client, urls []string, result chan<- string) {
+	for site := range message {
 
-	resp, err := c.Get(fmt.Sprintf("https://malshare.com/daily/%s/malshare_fileList.%s.all.txt", strArray[i], strArray[i]))
-	if err != nil {
-		// time.Sleep(time.Second * 10)
-		// makeRequest(i, message, c, strArray)
-		fmt.Println(err)
-		return
+		resp, err := c.Get(site.URL)
+		if err != nil {
+			time.Sleep(time.Second * 10)
+			// fmt.Println(err)
+			// return
+		}
+		// fmt.Println(resp.Body)
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// fmt.Println(j)
+		result <- string(body)
 	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	message <- string(body)
+	// fmt.Println(message)
+	// close(message)
+	// }
 
+}
+func connectMongoDb() context.Context {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://hoangdznka123:hoangdznka123@cluster0.okc3a4u.mongodb.net/?retryWrites=true&w=majority"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer client.Disconnect(ctx)
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		fmt.Println(err)
+	}
+	return ctx
 }
